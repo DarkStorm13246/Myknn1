faed51d22c1d7d84f5ef3207dc06f018e5b1fe1b
65/40-007 Thanawat Jibsamarnboon <155066338+DarkStorm13246@users.noreply.github.com>
2024-11-19 14:32:49 +0700

    Update home.py

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Website Developing using Python")
-st.header("Website Developing using Python")
+st.title("✖️Website Developing using Python✖️")
+st.header("🍔Website Developing using Python🍿")
 
 st.subheader("Thanawat Jibsamarnboon")
