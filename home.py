e913080641a26563ee620216df1ff622718da584
65/40-007 Thanawat Jibsamarnboon <155066338+DarkStorm13246@users.noreply.github.com>
2024-11-19 14:47:59 +0700

    Update home.py

--- home.py
+++ home.py
@@ -4,4 +4,9 @@ import pandas as pd
 st.title("✖️Website Developing using Python✖️")
 st.header("🍔Website Developing using Python🍿")
 
+st.image('./img/1.jpg')
 st.subheader("Thanawat Jibsamarnboon")
+
+dt=pd.read_csv('./data/iris-3csv')
+st.header()
+st.write(dt.head(10))
